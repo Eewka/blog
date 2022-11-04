c9f4a3982afcd6ae020253ec9ab5942fec5699cd
repl.it user <replituser@example.com>
2022-11-04 15:26:08 +0100

    Add

--- main.py
+++ main.py
@@ -20,6 +20,7 @@ gravatar = Gravatar(app, size=100, rating='g', default='retro', force_default=Fa
 # CONNECT TO DB
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL",  "sqlite:///blog.db")
 db = SQLAlchemy(app)
 login_manager = LoginManager()
 login_manager.init_app(app)
